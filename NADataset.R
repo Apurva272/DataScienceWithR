0235b5f36aab1eb9be6716c83bf583af9a73c37f
Apurva <61090951+Apurva272@users.noreply.github.com>
2024-03-21 23:27:19 +0530

    Add files via upload

--- NADataset.R
+++ NADataset.R
@@ -13,3 +13,18 @@ ind <- is.na(na_example)
 
 # Determine how many NA ind has using the sum function
 sum(ind)
+# Note what we can do with the ! operator
+x <- c(1, 2, 3)
+ind <- c(FALSE, TRUE, FALSE)
+x[!ind]
+
+# Create the ind vector
+library(dslabs)
+data(na_example)
+ind <- is.na(na_example)
+
+# We saw that this gives an NA
+mean(na_example)
+
+# Compute the average, for entries of na_example that are not NA 
+mean(na_example[!ind])
